4adbdc00b748a0af048412a15b89122c7102c880
Guillermo <mrog0@outlook.com>
2017-05-15 17:13:36 +0100

    Add maximum top up value Add deduct method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,11 +2,18 @@ class Oystercard
   
   attr_reader :balance
 
+  BALANCE_LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise "Maximum balance of £#{BALANCE_LIMIT} exceeded" if @balance + amount > BALANCE_LIMIT
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,10 +7,22 @@ describe Oystercard do
   
   describe "#top_up" do
 
-    it { is_expected.to respond_to(:top_up).with(1).argument }
-
-    it 'can top up the balance' do
+    it "can top up the balance" do
       expect{ subject.top_up(1) }.to change{ subject.balance }.to 1
+    end
+
+    it "raises an error if the maximum balance is exceeded." do
+      maximum_balance = Oystercard::BALANCE_LIMIT
+      subject.top_up(maximum_balance)
+      expect{ subject.top_up(1) }.to raise_error "Maximum balance of £#{maximum_balance} exceeded"
+    end
+  end
+
+  describe "#deduct" do
+
+    it "deducts an amount from the balance" do
+      subject.top_up(50)
+      expect { subject.deduct(1) }.to change { subject.balance }.by -1
     end
 
   end
